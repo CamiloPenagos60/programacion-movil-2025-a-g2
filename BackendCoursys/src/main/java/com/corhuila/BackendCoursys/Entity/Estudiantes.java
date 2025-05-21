8ba978e18cd2d3eb28e837ea1bbfbbce5a35553b
CamiloPenagos60 <camilopenagos617@gmail.com>
2025-05-20 23:36:22 -0500

    commit

---
+++ BackendCoursys/src/main/java/com/corhuila/BackendCoursys/Entity/Estudiantes.java
@@ -0,0 +1,68 @@
+package com.corhuila.BackendCoursys.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estudiantes")
+public class Estudiantes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre", length = 200, nullable = false)
+    private String nombre;
+
+    @Column(name = "cedula", length = 100, nullable = false)
+    private String cedula;
+
+    @Column(name = "telefono", nullable = false)
+    private String telefono;
+
+    @Column(name = "email", length = 200, nullable = false)
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
